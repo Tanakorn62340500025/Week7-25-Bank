db7733a5a390c773f34f2fab7f28358430522a29
Tanakorn62340500025 <78886841+Tanakorn62340500025@users.noreply.github.com>
2021-06-03 16:20:03 +0700

    completeforcodewriting

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,7 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include <math.h>
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -42,6 +42,7 @@
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart2;
 
@@ -56,6 +57,7 @@ float rpmwant = 0;
 float kp = 0;
 float ki = 0;
 float kd = 0;
+float pwmoutput;
 uint64_t Timestamp_Encoder = 0;
 /* USER CODE END PV */
 
@@ -65,6 +67,7 @@ static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 uint64_t micros();
 float EncoderVelocity_Update();
@@ -106,6 +109,7 @@ int main(void)
   MX_USART2_UART_Init();
   MX_TIM1_Init();
   MX_TIM2_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim2);
   HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_ALL);
@@ -124,7 +128,7 @@ int main(void)
 	  			//Timestamp_Encoder = micros();
 	  			//EncoderVel = EncoderVelocity_Update();
 	  		//}
-	  		//ค่านี้จะเก็บในตัวเเปร encodervel
+	  		//ค่านี้จะเ�?็บในตัวเเปร encodervel
 
 
 
@@ -134,18 +138,31 @@ int main(void)
 	  			Timestamp_Encoder = micros();
 	  			//EncoderVel = (EncoderVel * 99 + EncoderVelocity_Update()) / 100.0;
 	  			rpmencodervel = (EncoderVel*60/3072);
-	  			//errorต้องใช้absเนื่องจากกลัวมีปัญหากับการลบหรือบวกเลข
+	  			//errorต้องใช้absเนื่องจา�?�?ลัวมีปั�?หา�?ับ�?ารลบหรือบว�?เลข
 	  			error = fabs(rpmwant) - fabs(rpmencodervel);
-	  			//บวกเพื่อให้นำไปใส่ในสมการ PID ได้
+	  			//บว�?เพื่อให้นำไปใส่ในสม�?าร PID ได้
 	  			errorplus += error;
 	  			//PID equation
-	  			pwmoutput =
+	  			pwmoutput = (kp*error)+(ki*errorplus)+(kd*(error-errorbefore));
+
+	  			if(rpmwant < 0)   //เลขติดลบต้้องหมุน�?ลับทาง
+	  				            //rpmwant เนื่องจา�?มันเป็นค่าที่ใส่เข้ามาจึงบอ�?ทิศได้
+	  			{
+	  				__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 0);
+	  				__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, pwmoutput);
+	  			}
+
+	  			else if(rpmwant >= 0)     //ใส่ข้อมูลมาเป็นบวกเเปลว่ามอเตอร์หมุนทิศทางปกติ
+	  			{
+	  				__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pwmoutput);
+	  				__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+	  			}
 
 
-
-
-
-
+	  			errorbefore = error;
+	  			//save ค่า errorbefore = error เนื่องจา�?จะได้ save ค่า error ณ ปัจจุบันให้�?ลายเป็น error �?่อนหน้า
+	  			//หลังจา�?นั้นพอขึ้น loop ใหม่ error มันจะเปลี่ยนอี�? เเละนั้นเเหละ error before จะมาใช้ใน loop ถัดไปนั้น�?็คือเอาค่าของ loop �?่อนหน้ามานั้นเอง
+	  			Encodervel = (Encodervel*999 + EncoderVelocity_Update())/1000;
 	  		}
 	  		//low pass filter
   }
@@ -292,6 +309,70 @@ static void MX_TIM2_Init(void)
 }
 
 /**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 10000;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM2;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -373,14 +454,14 @@ float EncoderVelocity_Update()
 	//read data
 	uint32_t EncoderNowPosition = HTIM_ENCODER.Instance->CNT;
 	//ตำเเหน่งปัจจุบัน instance CNT เพื่อเปลี่ยนได้งง่าย
-	//TIM1->CNT เขียนได้เหมือนกับข้างบน
+	//TIM1->CNT เขียนได้เหมือน�?ับข้างบน
 	uint64_t EncoderNowTimestamp = micros();
-	//เวลาที่เกิดในปัจจุบันเป็นหน่วย micro พยายามเขียนให้ติดกันเพื่อระหว่าง ตำเเหน่งกับ เวลา ต้องเขียนให้ใกล้กันมากที่สุดเพราะมันต้องเกิดไล่เลี้ยกัน
+	//เวลาที่เ�?ิดในปัจจุบันเป็นหน่วย micro พยายามเขียนให้ติด�?ันเพื่อระหว่าง ตำเเหน่ง�?ับ เวลา ต้องเขียนให้ใ�?ล้�?ันมา�?ที่สุดเพราะมันต้องเ�?ิดไล่เลี้ย�?ัน
 
 	int32_t EncoderPositionDiff;
-	//ตำเเหน่งที่เป็นบวก ลบ ได้ เพราะจะได้หมุนไปด้าน ทวน หรือ ตามเข็มได้
+	//ตำเเหน่งที่เป็นบว�? ลบ ได้ เพราะจะได้หมุนไปด้าน ทวน หรือ ตามเข็มได้
 	uint64_t EncoderTimeDiff;
-	//เวลาเป็น unit ได้เพราะว่าเวลาเป็นบวกเสมอ
+	//เวลาเป็น unit ได้เพราะว่าเวลาเป็นบว�?เสมอ
 
 	EncoderTimeDiff = EncoderNowTimestamp - EncoderLastTimestamp;
 	//dt ดิฟเวลา
@@ -392,13 +473,13 @@ float EncoderVelocity_Update()
 	{
 		EncoderPositionDiff -= MAX_ENCODER_PERIOD;
 	}
-	//ค่าที่เรา diff ได้ ติดลบของติดลบ 2900 มันมากกว่าใน case นี้ก็ทำตาม -= ตาม case ด้านบนไป
+	//ค่าที่เรา diff ได้ ติดลบของติดลบ 2900 มันมา�?�?ว่าใน case นี้�?็ทำตาม -= ตาม case ด้านบนไป
 	else if (-EncoderPositionDiff >= MAX_SUBPOSITION_OVERFLOW)
 	{
 		EncoderPositionDiff += MAX_ENCODER_PERIOD;
 	}
-	//encoder หมุนกลับหลัง
-	//เป็นเหตุผลที่จะต้องเพิ่ม counter period สมมติ counter 60000 กว่าๆก็จะทำให้มันดีขึ้นเเละ overflow ได้ช้ากว่า
+	//encoder หมุน�?ลับหลัง
+	//เป็นเหตุผลที่จะต้องเพิ่ม counter period สมมติ counter 60000 �?ว่าๆ�?็จะทำให้มันดีขึ้นเเละ overflow ได้ช้า�?ว่า
 
 	//Update Position and time
 	EncoderLastPosition = EncoderNowPosition;
@@ -410,7 +491,7 @@ float EncoderVelocity_Update()
 
 }
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)    //callback เพื่อป้องกัน 	overflow
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)    //callback เพื่อป้อง�?ัน 	overflow
 {
 	if (htim == &htim2)
 	{
